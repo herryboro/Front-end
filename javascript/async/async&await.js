f38ae208686c70038fde7fa2435dee855ef6675d
Herryboro <herryboro@naver.com>
2020-12-26 14:51:04 +0900

    javascript callback, promise, async 개념, javascript 미니 프로젝트 html, css 화면단 구성

---
+++ javascript/async/async&await.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// 1. async
+function fetchUser() {
+  return new Promise((resolve, reject) => {
+    resolve('ellie');
+  });
+}
+
+const user = fetchUser();
+user.then(console.log);
